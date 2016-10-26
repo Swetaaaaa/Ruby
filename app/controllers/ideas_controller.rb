b4a3644b6a24600c3582e526314f5afb9f467657
Shweta Stha <swetacresta30@gmail.com>
2016-10-26 09:32:47 +0000

    Ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,6 +7,17 @@ class IdeasController < ApplicationController
         @idea=Idea.create(idea_params)
         redirect_to_root_path
     end
+    def edit
+        @idea=Idea.find(params[:id])
+    end
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+            redirect_to_root_path
+        else
+            redirect_to_edit_idea_path(params[:id])
+        end
+    end
     private
      def idea_params
          params.require(:idea).permit(:description, :author)
