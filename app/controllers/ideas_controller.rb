74c44dfe6ae7a804fabbb0a83ac80d9215ce8192
Shweta Stha <swetacresta30@gmail.com>
2016-11-08 06:58:08 +0000

    Ideator

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,14 +1,16 @@
 class IdeasController < ApplicationController
     def index
-        @ideas = Idea.all
+        @ideas = Idea.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
     end
 
     def create
         @idea=Idea.create(idea_params)
         if @idea.valid?
+            flash[:success]= "Your idea has been posted!"
         else
+            flash[:alert]="Woops!Error"
         end
-        redirect_to_root_path
+        redirect_to root_path
     end
     def edit
         @idea=Idea.find(params[:id])
@@ -16,14 +18,17 @@ class IdeasController < ApplicationController
     def update
         @idea = Idea.find(params[:id])
         if @idea.update(idea_params)
+            flash[:success]="The idea has been updated"
             redirect_to root_path
         else
+            flash[:alert]="Woops!Error"
             redirect_to edit_idea_path(params[:id])
         end
     end
     def destroy
         @idea = Idea.find(params[:id])
         @idea.destroy
+        flash[:success]="Idea was successfullly deleted"
         redirect_to root_path
     end
     
